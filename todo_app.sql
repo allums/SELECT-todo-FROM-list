c9f4dc9812cbb44a755aeefa261efc0917e16871
Stephen <stephenlallum@gmail.com>
2016-04-17 12:22:03 -1000

    updated sql commands

--- todo_app.sql
+++ todo_app.sql
@@ -31,3 +31,39 @@ CREATE TABLE tasks (
 );
 
 ALTER TABLE tasks DROP COLUMN completed;
+
+ALTER TABLE tasks ADD COLUMN completed_at timestamp DEFAULT NULL;
+
+ALTER TABLE tasks ALTER COLUMN updated_at SET NOT NULL;
+
+ALTER TABLE tasks ALTER COLUMN updated_at SET DEFAULT NOW();
+
+INSERT INTO tasks (title, description, created_at, updated_at, completed) VALUES ('Study SQL', 'Complete this exercise', now());
+
+INSERT INTO tasks (title, description) VALUES ('Study PostgreSQL', 'Read all the documentation');
+
+SELECT * FROM tasks WHERE completed_at IS NULL;
+
+INSERT INTO tasks (title) VALUES ('Study SQL');
+
+SELECT title, description FROM tasks WHERE completed_at IS NULL;
+
+UPDATE tasks SET completed_at=now() WHERE title='Study SQL';
+
+SELECT * FROM tasks ORDER BY created_at DESC;
+
+INSERT INTO tasks (title, description) VALUE ('mistake 1', 'a test entry');
+
+INSERT INTO tasks (title, description) VALUE ('mistake 2', 'another test entry');
+
+INSERT INTO tasks (title, description) VALUE ('mistake 3', 'another test entry');
+
+SELECT * FROM tasks WHERE title LIKE '%mistake%';
+
+DELETE FROM tasks WHERE title='mistake 1';
+
+SELECT title, description FROM tasks WHERE title LIKE '%mistake%';
+
+DELETE FROM tasks WHERE title LIKE '%mistake%';
+
+SELECT FROM tasks ORDER BY title ASC;
